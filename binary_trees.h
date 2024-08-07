ee01ce28c03bcdd1b446533d43249b23badcc477
umudovafatima <umudovafatimee@gmail.com>
2024-08-07 11:37:47 +0000

    task5_trial

--- binary_trees.h
+++ binary_trees.h
@@ -29,5 +29,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
