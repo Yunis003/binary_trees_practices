d1573b24b7556472ece063ad24b43e6b3fd28125
Yunis003 <hi.yunisqulamov@gmail.com>
2024-08-07 18:59:41 +0000

    task18_trial5

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *new_node;
 
-	if (node == NULL || node->parent == NULL || node->parent->parent)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
