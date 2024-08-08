a216926c49db0da0fb00678b9b48d383922def72
umudovafatima <umudovafatimee@gmail.com>
2024-08-08 14:04:30 +0000

    updated_task13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,16 +10,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int count = 0;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 	if (binary_tree_nodes(tree->left) != 0 || binary_tree_nodes(tree->right) != 0)
 	{
-		return (count + 1);
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
 	else
-		return (count);
+		return (0);
 }
